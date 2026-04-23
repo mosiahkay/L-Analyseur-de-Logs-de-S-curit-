0b02e6e2fc7d9146ebd10fc21e9b74376730a590
mosiah kay <mosiahkayumba@gmail.com>
2026-04-23 20:30:05 +0200

    Add log analysis functionality to count login errors per user

---
+++ main.py
@@ -0,0 +1,37 @@
+import re
+
+logs_bruts = [
+    "User: admin | Action: LOGIN_FAILED",
+    "User: guest | Action: LOGIN_SUCCESS",
+    "User: admin | Action: LOGIN_FAILED",
+    "User: hacker | Action: LOGIN_FAILED",
+    "User: admin | Action: LOGIN_FAILED",
+    "User: hacker | Action: LOGIN_FAILED"
+]
+
+compteur_erreurs = {}
+
+"""on va commencer a par verifier si la cle existe"""
+
+tentative = {}
+
+user = "admin"
+if user not in tentative:
+    """on initialise a 0 s'il n'existe pas"""
+    tentative[user] = 0 
+tentative[user] += 1 #maintenant on incremente le compteur de tentative pour l'utilisateur "admin"
+
+for log in logs_bruts:
+    """On va parcourir chaque log et extraire le nom de l'utilisateur
+    on commence par definir un motif de regex pour extraire le nom de l'utilisateur"""
+    motif = r"User: (\w+)"
+    match = re.search(motif, log)
+    if match:
+        user = match.group(1) #on extrait le nom de l'utilisateur le group(1) recupere ce qui est entre les parenthese dans le motif regex
+        if user not in compteur_erreurs:
+            compteur_erreurs[user] = 0 #on initialise le compteur d'erreurs pour cet utilisateur à 0
+            if "LOGIN_FAILED" in log:
+                compteur_erreurs[user] += 1 #on incremente le compteur d'erreurs pour cet utilisateur 
+# Affichage du nombre d'erreurs de connexion pour chaque utilisateur
+for user, erreurs in compteur_erreurs.items():
+    print(f"Utilisateur: {user}, Erreurs de connexion: {erreurs}")
